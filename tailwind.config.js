de3c0adb14f81ca948115a7efc38d319a40f5955
elvisuriel <urielmarciales@gmil.com>
2024-08-13 17:00:25 -0500

    ajustando estilos implementando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/landing/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
