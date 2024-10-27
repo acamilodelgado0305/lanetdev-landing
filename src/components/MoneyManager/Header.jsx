a30cf0692db3a0d7b913f3d1208d17f41a7d9c32
Camilo Delgado <camilodelgado0305@gmail.com>
2024-10-27 16:49:46 -0500

    barra de money manager

--- src/components/MoneyManager/Header.jsx
+++ src/components/MoneyManager/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
 
   return (
     <header className="bg-white shadow-sm w-full">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-14">
           {/* Logo */}
           <div className="flex items-center">
