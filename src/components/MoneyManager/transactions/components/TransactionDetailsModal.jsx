4f5d3f3675ffa62b8bcb42cefcf0c36639d329f6
elvisuriel <urielmarciales@gmil.com>
2024-11-22 10:51:48 -0500

    ajustes

--- src/components/MoneyManager/transactions/components/TransactionDetailsModal.jsx
+++ src/components/MoneyManager/transactions/components/TransactionDetailsModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button, Modal, message } from "antd";
+import { Button, Modal, Input, message } from "antd";
 import {
     CloseOutlined,
     EditOutlined,
@@ -7,6 +7,7 @@ import {
     FileTextOutlined,
     PrinterOutlined,
     ExclamationCircleOutlined,
+    SaveOutlined,
 } from "@ant-design/icons";
 
 import { getUserById } from "../../../../services/apiService";
@@ -22,6 +23,8 @@ const TransactionDetailModal = ({
     formatCurrency,
 }) => {
     const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
+    const [isEditMode, setEditMode] = useState(false); // Estado para modo edición
+    const [editedEntry, setEditedEntry] = useState({});
     const [userName, setUserName] = useState("Cargando...");
     const { authToken } = useAuth();
     const API_BASE_URL = import.meta.env.VITE_API_FINANZAS;
@@ -29,6 +32,9 @@ const TransactionDetailModal = ({
     useEffect(() => {
         if (isOpen && entry?.user_id && authToken) {
             fetchUserName(entry.user_id, authToken);
+        }
+        if (isOpen) {
+            setEditedEntry(entry || {});
         }
     }, [isOpen, entry, authToken]);
 
@@ -41,17 +47,15 @@ const TransactionDetailModal = ({
             setUserName("Desconocido");
         }
     };
-
     const showDeleteModal = () => {
         setDeleteModalOpen(true);
     };
 
     const handleDelete = async () => {
         try {
-            const response = await axios.delete(`${API_BASE_URL}/incomes/${entry.id}`);
+            const response = await axios.delete(`${API_BASE_URL}/incomes/${entry.id}`, {
+            });
             if (response.status === 200 || response.status === 204) {
-                console.log("Ingreso eliminado:", entry.id);
-
                 message.success("Ingreso eliminado con éxito.");
                 setDeleteModalOpen(false);
                 onClose();
@@ -63,8 +67,53 @@ const TransactionDetailModal = ({
             message.error("Hubo un error al intentar eliminar el ingreso. Por favor, inténtalo de nuevo.");
         }
     };
+
     const handleCancelDelete = () => {
         setDeleteModalOpen(false);
+    };
+
+    const toggleEditMode = () => {
+        setEditMode((prev) => !prev);
+    };
+
+    const handleInputChange = (field, value) => {
+        setEditedEntry((prev) => ({
+            ...prev,
+            [field]: value,
+        }));
+    };
+
+    const handleSaveChanges = async () => {
+        try {
+            console.log("Datos enviados para la actualización:", editedEntry);
+            const formattedEntry = {
+                ...editedEntry,
+                amount: parseFloat(editedEntry.amount),
+                amountfev: parseFloat(editedEntry.amountfev),
+                amountdiverse: parseFloat(editedEntry.amountdiverse),
+                estado: editedEntry.estado === "Activo" || editedEntry.estado === true,
+            };
+
+            // Validación antes de enviar
+            if (isNaN(formattedEntry.amount) || formattedEntry.amount <= 0) {
+                message.error("El monto debe ser un número positivo....");
+                return;
+            }
+            const response = await axios.put(
+                `${API_BASE_URL}/incomes/${entry.id}`,
+                editedEntry,
+            );
+            if (response.status === 200) {
+                message.success("Ingreso actualizado con éxito.");
+                setEditMode(false);
+                onClose();
+            } else {
+                throw new Error("Error inesperado al actualizar el ingreso.");
+            }
+        } catch (error) {
+            console.error("Error actualizando el ingreso:", error);
+            message.error("Hubo un error al intentar actualizar el ingreso. Por favor, inténtalo de nuevo.");
+        }
     };
 
     if (!isOpen || !entry) return null;
@@ -78,7 +127,9 @@ const TransactionDetailModal = ({
                         <div className="bg-blue-100 p-2 rounded-full">
                             <FileTextOutlined className="text-blue-500 text-xl" />
                         </div>
-                        <h2 className="text-xl font-semibold">Detalle del Ingreso</h2>
+                        <h2 className="text-xl font-semibold">
+                            {isEditMode ? "Editar Ingreso" : "Detalle del Ingreso"}
+                        </h2>
                     </div>
                     <Button
                         type="text"
@@ -89,70 +140,122 @@ const TransactionDetailModal = ({
                 </div>
                 <hr className="border-t-2 border-blue-500 mb-4" />
 
-                {/* Descripción principal */}
+                {/* Formulario editable */}
                 <div>
                     <p className="text-xl font-semibold">Usuario</p>
                     <p className="font-medium">{userName || "Desconocido"}</p>
                 </div>
-
-
-                {/* Detalles del ingreso */}
                 <div className="bg-gray-50 p-4 rounded-lg my-4">
-                    <p className="text-xl font-bold text-gray-800">
-                        Monto total: {formatCurrency(entry.amount)}
-                        <span className="ml-4 px-2 py-1 text-sm text-blue-600 bg-blue-100 rounded">
-                            {entry.estado ? "Activo" : "Inactivo"}
-                        </span>
-                    </p>
-                    <div className="grid grid-cols-2 gap-4 mt-4">
+                    <div className="grid grid-cols-2 gap-4">
                         <div>
-                            <p className="text-sm text-gray-500">Fecha y hora</p>
-                            <p className="font-medium">
-                                {new Date(entry.date).toLocaleString()}
-                            </p>
+                            <p className="text-sm text-gray-500">Monto</p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.amount}
+                                    onChange={(e) => handleInputChange("amount", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium">{formatCurrency(entry.amount)}</p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Descripción</p>
-                            <p className="font-medium">{entry.description}</p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.description}
+                                    onChange={(e) => handleInputChange("description", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium">{entry.description}</p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Categoría</p>
-                            <p className="font-medium">
-                                {getCategoryName(entry.category_id)}
-                            </p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.category_id}
+                                    onChange={(e) => handleInputChange("category_id", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium">
+                                    {getCategoryName(entry.category_id)}
+                                </p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Cuenta</p>
-                            <p className="font-medium">
-                                {getAccountName(entry.account_id)}
-                            </p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.account_id}
+                                    onChange={(e) => handleInputChange("account_id", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium">
+                                    {getAccountName(entry.account_id)}
+                                </p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Monto FEV</p>
-                            <p className="font-medium text-green-600">
-                                {formatCurrency(entry.amountfev || 0)}
-                            </p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.amountfev}
+                                    onChange={(e) => handleInputChange("amountfev", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium text-green-600">
+                                    {formatCurrency(entry.amountfev || 0)}
+                                </p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Monto Diverse</p>
-                            <p className="font-medium text-green-600">
-                                {formatCurrency(entry.amountdiverse || 0)}
-                            </p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.amountdiverse}
+                                    onChange={(e) => handleInputChange("amountdiverse", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium text-green-600">
+                                    {formatCurrency(entry.amountdiverse || 0)}
+                                </p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Tipo</p>
-                            <p className="font-medium">{entry.type || "Desconocido"}</p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.type}
+                                    onChange={(e) => handleInputChange("type", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium">{entry.type || "Desconocido"}</p>
+                            )}
                         </div>
                         <div>
                             <p className="text-sm text-gray-500">Estado</p>
-                            <p className="font-medium">
-                                {entry.estado ? "Activo" : "Inactivo"}
-                            </p>
+                            {isEditMode ? (
+                                <Input
+                                    value={editedEntry.estado ? "Activo" : "Inactivo"}
+                                    onChange={(e) => handleInputChange("estado", e.target.value)}
+                                />
+                            ) : (
+                                <p className="font-medium">
+                                    {entry.estado ? "Activo" : "Inactivo"}
+                                </p>
+                            )}
                         </div>
                         {entry.note && (
                             <div className="col-span-2">
                                 <p className="text-sm text-gray-500">Nota</p>
-                                <p className="font-medium">{entry.note}</p>
+                                {isEditMode ? (
+                                    <Input
+                                        value={editedEntry.note}
+                                        onChange={(e) => handleInputChange("note", e.target.value)}
+                                    />
+                                ) : (
+                                    <p className="font-medium">{entry.note}</p>
+                                )}
                             </div>
                         )}
                     </div>
@@ -176,14 +279,24 @@ const TransactionDetailModal = ({
                     >
                         <span className="text-sm mt-1 font-semibold">Comprobante</span>
                     </Button>
-                    <Button
-                        className="flex flex-col items-center text-yellow-600 bg-yellow-100 hover:bg-yellow-200"
-                        icon={
-                            <EditOutlined style={{ fontSize: "2rem", color: "#ca8a04" }} />
-                        }
-                    >
-                        <span className="text-sm mt-1 font-semibold text-yellow-600">Editar</span>
-                    </Button>
+                    {isEditMode ? (
+
+                        <Button
+                            className="flex flex-col items-center text-green-600 hover:text-green-800"
+                            icon={<SaveOutlined style={{ fontSize: "2rem", color: "#16a34a" }} />}
+                            onClick={handleSaveChanges}
+                        >
+                            <span className="text-sm mt-1 font-semibold">Guardar</span>
+                        </Button>
+                    ) : (
+                        <Button
+                            className="flex flex-col items-center text-yellow-600 bg-yellow-100 hover:bg-yellow-200"
+                            icon={<EditOutlined style={{ fontSize: "2rem", color: "#ca8a04" }} />}
+                            onClick={toggleEditMode}
+                        >
+                            <span className="text-sm mt-1 font-semibold text-yellow-600">Editar</span>
+                        </Button>
+                    )}
                     <Button
                         danger
                         className="flex flex-col items-center text-red-600 hover:text-red-800"
@@ -196,6 +309,7 @@ const TransactionDetailModal = ({
                     </Button>
                 </div>
             </div>
+
 
             {/* Modal de confirmación de eliminación */}
             <Modal
