ad592f29f1a0551fe9b22b55d706d92981b1cdb1
elvisuriel <urielmarciales@gmil.com>
2025-01-21 20:45:39 -0500

    ajusts

--- src/components/MoneyManager/transactions/components/ExpenseTable.jsx
+++ src/components/MoneyManager/transactions/components/ExpenseTable.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
-import { Button, Tooltip, Dropdown, Menu, Drawer } from 'antd';
+import { Button, Table, Tooltip, Dropdown, Menu, Drawer } from 'antd';
 import { format as formatDate } from 'date-fns';
-import {
-    FileTextOutlined,
-    FilterOutlined,
-    CaretDownOutlined
-} from '@ant-design/icons';
+import { FilterOutlined, CaretDownOutlined } from '@ant-design/icons';
 import _ from 'lodash';
 import ExpenseDetailModal from './ExpenseDetailModal';
 
@@ -15,12 +11,11 @@ const ExpenseTable = ({
     accounts = [],
     onDelete,
 }) => {
-    const [columnFilters, setColumnFilters] = useState({});
-    const [hoveredRow, setHoveredRow] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedEntry, setSelectedEntry] = useState(null);
     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
     const [selectedImages, setSelectedImages] = useState([]);
+
     const formatCurrency = (amount) => {
         return new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -39,106 +34,153 @@ const ExpenseTable = ({
         return account ? account.name : "Cuenta no encontrada";
     };
 
-    const getUniqueValues = (field) => {
-        return _.uniq(entries.map(entry => {
-            switch (field) {
-                case 'category':
-                    return getCategoryName(entry.category_id);
-                case 'account':
-                    return entry.entryType === "transfer"
-                        ? `${getAccountName(entry.from_account_id)} ➡️ ${getAccountName(entry.to_account_id)}`
-                        : getAccountName(entry.account_id);
-                case 'tax_type':
-                    return entry.tax_type || 'No aplica';
-                default:
-                    return entry[field];
-            }
-        })).filter(Boolean);
-    };
-
-    const getFilterMenu = (field) => (
-        <Menu
-            items={getUniqueValues(field).map(value => ({
-                key: value,
-                label: (
-                    <div className="flex items-center">
-                        <input
-                            type="checkbox"
-                            checked={columnFilters[field]?.includes(value)}
-                            onChange={(e) => {
-                                let newFilters = { ...columnFilters };
-                                if (!newFilters[field]) newFilters[field] = [];
-                                if (e.target.checked) {
-                                    newFilters[field] = [...newFilters[field], value];
-                                } else {
-                                    newFilters[field] = newFilters[field].filter(v => v !== value);
-                                }
-                                if (newFilters[field].length === 0) delete newFilters[field];
-                                setColumnFilters(newFilters);
-                            }}
-                        />
-                        <span className="ml-2">{value}</span>
-                    </div>
-                ),
-            }))}
-        />
-    );
-
     const columns = [
         {
             title: 'Fecha',
-            field: 'date',
+            dataIndex: 'date',
+            key: 'date',
             width: '100px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => formatDate(new Date(entry.date), "d MMM yyyy")))].map((formattedDate) => ({
+                text: formattedDate,
+                value: formattedDate,
+            })),
+            render: (text) => formatDate(new Date(text), "d MMM yyyy"),
+            sorter: (a, b) => new Date(a.date) - new Date(b.date), // Orden cronológico
+            sortDirections: ["descend", "ascend"], // De más reciente a más antiguo
         },
         {
             title: 'Descripción',
-            field: 'description',
-            width: '200px',
+            dataIndex: "description",
+            key: "description",
+            width: '250px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => entry.description))].map((description) => ({
+                text: description,
+                value: description,
+            })),
+            onFilter: (value, record) => record.description.includes(value),
+            sorter: (a, b) => getAccountName(a.description).localeCompare(getAccountName(b.description)),
+            sortDirections: ["ascend", "descend"],
         },
         {
             title: 'Cuenta',
-            field: 'account',
+            dataIndex: "account_id",
+            key: "account_id",
             width: '150px',
+            filterSearch: true,
+            render: (id) => getAccountName(id),
+            filters: [...new Set(entries.map((entry) => getAccountName(entry.account_id)))].map((name) => ({
+                text: name,
+                value: name,
+            })),
+            onFilter: (value, record) => getAccountName(record.account_id) === value,
+            sortDirections: ["ascend", "descend"],
         },
         {
             title: 'Categoría',
-            field: 'category',
+            dataIndex: 'category_id',
+            key: 'category_id',
             width: '150px',
+            filterSearch: true,
+            render: (id) => getCategoryName(id),
+            filters: [...new Set(entries.map((entry) => getCategoryName(entry.category_id)))].map((name) => ({
+                text: name,
+                value: name,
+            })),
+            onFilter: (value, record) => getCategoryName(record.category_id) === value,
+            sortDirections: ["ascend", "descend"],
         },
         {
             title: 'Monto',
-            field: 'amount',
-            width: '120px',
+            dataIndex: 'amount',
+            key: "amount",
+            width: '150px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => entry.amount))].map((amount) => ({
+                text: formatCurrency(amount),
+                value: amount,
+            })),
+            onFilter: (value, record) => record.amount === value,
+            render: (amount) => formatCurrency(amount),
+            sorter: (a, b) => a.amount - b.amount,
+            sortDirections: ["descend", "ascend"],
         },
         {
             title: 'Impuestos',
-            field: 'tax_type',
+            dataIndex: 'tax_type',
+            key: 'tax_type',
             width: '120px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => entry.tax_type))].map((taxType) => ({
+                text: taxType,
+                value: taxType,
+            })),
+            onFilter: (value, record) => record.tax_type === value,
+            sorter: (a, b) => a.amount - b.amount,
+            sortDirections: ["ascend", "descend"],
         },
         {
             title: 'Proveedor',
-            field: 'provider_id',
+            dataIndex: 'provider_id',
             width: '150px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => entry.provider_id))].map((provider) => ({
+                text: provider,
+                value: provider,
+            })),
+            onFilter: (value, record) => record.provider_id === value,
+            sorter: (a, b) => a.amount - b.amount,
+            sortDirections: ["ascend", "descend"],
         },
         {
             title: 'Retención',
-            field: 'amount',
+            dataIndex: 'retention_type',
+            key: 'retention_type',
             width: '120px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => entry.retention_type))].map((retention) => ({
+                text: retention,
+                value: retention,
+            })),
+            onFilter: (value, record) => record.retention_type === value,
+            sorter: (a, b) => a.amount - b.amount,
+            sortDirections: ["ascend", "descend"],
         },
         {
             title: 'Estado',
-            field: 'recurrent',
+            dataIndex: 'recurrent',
             width: '120px',
+            filterSearch: true,
+            filters: [...new Set(entries.map((entry) => entry.recurrent))].map((status) => ({
+                text: status,
+                value: status,
+            })),
+            onFilter: (value, record) => (record.recurrent ? 'Sí' : 'No') === value,
+            sorter: (a, b) => a.amount - b.amount,
+            sortDirections: ["ascend", "descend"],
         },
         {
-            title: 'Comprobante',
-            field: 'voucher',
-            width: '120px',
+            title: "Comprobante",
+            dataIndex: "voucher",
+            key: "voucher",
+            filterSearch: true,
+            render: (vouchers) =>
+                Array.isArray(vouchers) && vouchers.length > 0 ? (
+                    <a
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            openDrawer(vouchers);
+                        }}
+                        className="text-blue-500 underline"
+                    >
+                        Ver comprobante
+                    </a>
+                ) : (
+                    "—"
+                ),
         },
-
-
     ];
-
     const openModal = (entry) => {
         setSelectedEntry(entry);
         setIsModalOpen(true);
@@ -157,133 +199,21 @@ const ExpenseTable = ({
         setIsDrawerOpen(true);
     };
 
+
     return (
         <>
             <div className="overflow-auto h-[39em]">
-                <table className="w-full relative table-fixed border-collapse">
-                    <thead className="sticky top-0 z-5 bg-white">
-                        <tr className="border-b border-gray-200">
-                            {columns.map((column) => (
-                                <th
-                                    key={column.field}
-                                    style={{ width: column.width }}
-                                    className="border-r border-gray-200 bg-gray-50 p-0"
-                                >
-                                    <div className="flex items-center justify-between px-3 py-2">
-                                        <span className="text-xs font-medium text-gray-500">{column.title}</span>
-                                        {column.field !== 'actions' && (
-                                            <Dropdown
-                                                overlay={getFilterMenu(column.field)}
-                                                trigger={['click']}
-                                            >
-                                                <Button
-                                                    type="text"
-                                                    size="small"
-                                                    className={`flex items-center ${columnFilters[column.field]?.length ? 'text-blue-600' : 'text-gray-400'}`}
-                                                    icon={<FilterOutlined />}
-                                                >
-                                                    <CaretDownOutlined style={{ fontSize: '10px' }} />
-                                                </Button>
-                                            </Dropdown>
-                                        )}
-                                    </div>
-                                </th>
-                            ))}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {entries.filter(entry => {
-                            return Object.entries(columnFilters).every(([field, values]) => {
-                                let cellValue;
-                                switch (field) {
-                                    case 'category':
-                                        cellValue = getCategoryName(entry.category_id);
-                                        break;
-                                    case 'account':
-                                        cellValue = entry.entryType === "transfer"
-                                            ? `${getAccountName(entry.from_account_id)} ➡️ ${getAccountName(entry.to_account_id)}`
-                                            : getAccountName(entry.account_id);
-                                        break;
-                                    case 'tax_type':
-                                        cellValue = entry.tax_type || 'No aplica';
-                                        break;
-                                    default:
-                                        cellValue = entry[field];
-                                }
-                                return values.includes(cellValue);
-                            });
-                        }).map((entry, rowIndex) => (
-                            <Tooltip key={rowIndex} title={`Fila: ${rowIndex + 1}, Descripción: ${entry.description}`} placement="top">
-                                <tr
-                                    className={`border-b hover:bg-gray-50 ${hoveredRow === rowIndex ? 'bg-gray-50' : ''}`}
-                                    onMouseEnter={() => setHoveredRow(rowIndex)}
-                                    onMouseLeave={() => setHoveredRow(null)}
-                                    onClick={() => openModal(entry)}
-                                    style={{ cursor: "pointer" }}
-                                >
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {formatDate(new Date(entry.date), "d MMM yyyy")}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {entry.description}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {entry.entryType === "transfer"
-                                            ? `${getAccountName(entry.from_account_id)} ➡️ ${getAccountName(entry.to_account_id)}`
-                                            : getAccountName(entry.account_id)}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {getCategoryName(entry.category_id)}
-                                    </td>
-                                    <td className={"border-r border-gray-200 p-2 truncate font-medium text-red-600"}>
-                                        {entry.type === "expense" ? "-" : "-"}
-                                        {formatCurrency(entry.amount)}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {entry.tax_type === "IVA" ? (
-                                            <div>
-                                                <span>IVA: </span>
-                                                <span className="text-green-600">
-                                                    {formatCurrency(entry.amount * 0.19)}
-                                                </span>
-                                            </div>
-                                        ) : (
-                                            "No aplica"
-                                        )}
-                                    </td>
+                <Table
+                    dataSource={entries}
+                    columns={columns}
+                    rowKey={(record) => record.id}
+                    pagination={{ pageSize: 10 }}
+                    bordered
+                    onRow={(record) => ({
+                        onClick: () => openModal(record),
+                    })}
 
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {entry.provider_id}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {entry.retention_type}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {entry.recurrent ? "Si" : "No"}
-                                    </td>
-                                    <td className="border-r border-gray-200 p-2 truncate">
-                                        {Array.isArray(entry.voucher) && entry.voucher.length > 0 ? (
-                                            <a
-                                                onClick={(e) => {
-                                                    e.stopPropagation();
-                                                    const voucherArray = entry.voucher.filter(
-                                                        (voucherUrl) => typeof voucherUrl === "string" && voucherUrl.trim() !== ""
-                                                    );
-                                                    openDrawer(voucherArray);
-                                                }}
-                                                className="text-blue-500 underline"
-                                            >
-                                                Ver comprobante
-                                            </a>
-                                        ) : (
-                                            "—"
-                                        )}
-                                    </td>
-                                </tr>
-                            </Tooltip>
-                        ))}
-                    </tbody>
-                </table>
+                />
             </div>
             <Drawer
                 visible={isDrawerOpen}
@@ -303,8 +233,8 @@ const ExpenseTable = ({
                                 />
                                 <Button
                                     type="link"
-                                    className="mx-20 absolute bottom-2   text-white bg-green-600"
-                                    onClick={() => downloadImage(image)} // Descarga individual
+                                    className="mx-20 absolute bottom-2 text-white bg-green-600"
+                                    onClick={() => downloadImage(image)}
                                 >
                                     Descargar
                                 </Button>
@@ -329,6 +259,7 @@ const ExpenseTable = ({
                 getCategoryName={getCategoryName}
                 getAccountName={getAccountName}
                 formatCurrency={formatCurrency}
+                onDelete={onDelete}
             />
         </>
     );
